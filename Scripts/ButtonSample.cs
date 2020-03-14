0683a61eb5b8b57af586fcdc27ddd23a3ad1ba97
Porrith Suong <psuong95@gmail.com>
2020-03-14 17:30:16 -0400

    Adds a Button Sample to test out producing.

---
+++ Scripts/ButtonSample.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using Unity.Entities;
+
+namespace UGUIDots.Controls.Authoring {
+
+    public struct SampleID : IComponentData {
+        public int Value;
+    }
+
+    public class ButtonSample : MonoBehaviour, IConvertGameObjectToEntity {
+
+        public int Value;
+
+        public void Convert(Entity entity, EntityManager dstManager, GameObjectConversionSystem conversionSystem) {
+            var msgEntity = dstManager.CreateEntity();
+            dstManager.AddComponentData(msgEntity, new SampleID { Value = Value });
+
+            dstManager.AddComponentData(entity, new ButtonMessageFramePayload { Value = msgEntity });
+
+#if UNITY_EDITOR
+            dstManager.SetName(msgEntity, $"{this.name} Button Payload Template");
+#endif
+        }
+    }
+}
