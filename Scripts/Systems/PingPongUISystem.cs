1a85556dd179c3eed9609a8ead24be529715056d
Porrith Suong <psuong95@gmail.com>
2020-04-15 09:46:54 -0400

    Candidate working idea of a translation based shader instead.

--- Scripts/Systems/PingPongUISystem.cs
+++ Scripts/Systems/PingPongUISystem.cs
@@ -3,53 +3,64 @@ using Unity.Core;
 using Unity.Entities;
 using Unity.Mathematics;
 using Unity.Transforms;
+using UnityEngine;
 
 namespace UGUIDots.Sample.Systems {
 
-    [UpdateInGroup(typeof(PresentationSystemGroup))]
+    // TODO: Switch to copying this over to the shader
+    [UpdateInGroup(typeof(UITransformUpdateGroup))]
     public class PingPongUISystem : SystemBase {
 
         private EntityCommandBufferSystem cmdBufferSystem;
+
+        private readonly int Translation = Shader.PropertyToID("_Translation");
 
         protected override void OnCreate() {
             cmdBufferSystem = World.GetOrCreateSystem<BeginPresentationEntityCommandBufferSystem>();
         }
 
         protected unsafe override void OnUpdate() {
-            var ltws = GetComponentDataFromEntity<LocalToWorld>(true);
-            var ltps = GetComponentDataFromEntity<LocalToParent>(false);
-            var translations = GetComponentDataFromEntity<Translation>(false);
-            var cmdBuffer = cmdBufferSystem.CreateCommandBuffer();
+            var ltws         = GetComponentDataFromEntity<LocalToWorld>(true);
+            var parents      = GetComponentDataFromEntity<Parent>(true);
+            var cmdBuffer    = cmdBufferSystem.CreateCommandBuffer();
 
             TimeData* time = stackalloc TimeData[1] { Time };
 
-            Entities.ForEach((Entity e, DynamicBuffer<Float4MaterialPropertyParam> b0, ref Parent c1, ref PingPongPositions c0) => {
+            Entities.ForEach((Entity e, ref PingPongPositions c0, ref Translation c2, ref LocalToParent c3, in MaterialPropertyIndex c1) => {
                 var position = ltws[e].Position.xy;
-                if (math.distance(position, c0.LHS) <= 0.1f) {
+                if (math.distance(position, c0.LHS) <= 0.2f) {
                     c0.Target = c0.RHS;
                 }
 
-                if (math.distance(position, c0.RHS) <= 0.1f) {
+                if (math.distance(position, c0.RHS) <= 0.2f) {
                     c0.Target = c0.LHS;
                 }
 
                 position = math.lerp(position.xy, c0.Target, time->DeltaTime);
 
+                var parent = parents[e].Value;
+
                 var worldSpace = float4x4.TRS(new float3(position, 0), quaternion.identity, new float3(1));
-                var localSpace = new LocalToParent { Value = math.mul(math.inverse(ltws[c1.Value].Value), worldSpace) };
+                var localSpace = new LocalToParent { Value = math.mul(math.inverse(ltws[parent].Value), worldSpace) };
 
-                ltps[e]         = localSpace;
-                translations[e] = new Translation { Value = localSpace.Position };
+                var root = RecurseGetRoot(parents, e);
 
-                var property = b0[0].ID;
+                var batch = EntityManager.GetComponentData<MaterialPropertyBatch>(root);
+                var property = batch.Value[c1.Value];
+                property.SetVector(Translation, new float4(localSpace.Position, 0));
 
-                b0[0] = new Float4MaterialPropertyParam(
-                    property,
-                    new float4(localSpace.Position, 1)
-                );
+                c3 = localSpace;
+                c2 = new Translation { Value = localSpace.Position };
 
-                cmdBuffer.AddComponent(e, new BuildUIElementTag { });
-            }).WithNativeDisableUnsafePtrRestriction(time).Run();
+            }).WithNativeDisableUnsafePtrRestriction(time).WithoutBurst().Run();
+        }
+
+        private Entity RecurseGetRoot(ComponentDataFromEntity<Parent> parents, Entity child) {
+            if (parents.Exists(child)) {
+                return RecurseGetRoot(parents, parents[child].Value);
+            }
+
+            return child;
         }
     }
 }
