cd30d8c1fb7330c025670f31afa2f5e90b781af3
Porrith Suong <psuong95@gmail.com>
2020-04-15 19:38:32 -0400

    Adds a MaxThreadCount script to not spam the # of threads.

---
+++ Scripts/MaxThreadCount.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using Unity.Jobs.LowLevel.Unsafe;
+
+namespace UGUIDots {
+
+    public class MaxThreadCount : MonoBehaviour {
+
+        public int JobWorkerCount = 2;
+        
+        private void Awake() {
+            JobsUtility.JobWorkerCount = JobWorkerCount;
+        }
+    }
+}
