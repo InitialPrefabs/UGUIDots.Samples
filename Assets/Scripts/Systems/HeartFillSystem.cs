3d5d797311b5c85e3d661be6db34953f62fbcfa2
Porrith <porrith.suong@gmail.com>
2020-11-15 13:49:34 -0500

    Removes unnecessary imports from the HeartFillSystem.

--- Assets/Scripts/Systems/HeartFillSystem.cs
+++ Assets/Scripts/Systems/HeartFillSystem.cs
@@ -1,16 +1,11 @@
-using UGUIDOTS.Render;
-using UGUIDOTS.Transforms;
 using Unity.Entities;
 using Unity.Mathematics;
-using Unity.Transforms;
 
 namespace UGUIDOTS.Sample.Systems {
 
     public class HeartFillSystem : SystemBase {
 
         protected override void OnUpdate() {
-            var parents = GetComponentDataFromEntity<Unity.Transforms.Parent>(true);
-
             float elapsedTime = (float)Time.ElapsedTime;
 
             Entities.WithAll<SpriteData>().ForEach((ref AxisFillAmount c0) => {
