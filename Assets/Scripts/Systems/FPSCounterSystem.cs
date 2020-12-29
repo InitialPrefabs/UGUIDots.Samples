9a39927fdb470abb553d2852af66f70275aaa001
Porrith <porrith.suong@gmail.com>
2020-12-29 10:45:07 -0500

    Updates FPSCounterSystem to resize and replace the char buffer.

--- Assets/Scripts/Systems/FPSCounterSystem.cs
+++ Assets/Scripts/Systems/FPSCounterSystem.cs
@@ -2,6 +2,7 @@ using UGUIDOTS.Common;
 using Unity.Entities;
 
 namespace UGUIDOTS.Sample.Systems {
+
     public class FPSCounterSystem : SystemBase {
         protected unsafe override void OnUpdate() {
             var timeData = Time.DeltaTime;
@@ -9,8 +10,10 @@ namespace UGUIDOTS.Sample.Systems {
             Entities.WithAll<DynamicTextTag>().ForEach((DynamicBuffer<CharElement> b0) => {
                 int fps = (int)(1f / timeData);
 
+                b0.ResizeAndReplaceBufferWithChars(fps);
+
                 char* ptr = (char*)b0.GetUnsafePtr();
-                fps.ToCharArray(ptr, b0.Length, out int count);
+                fps.ToChars(ptr, b0.Length, out int count);
             }).Run();
         }
     }
